a33774a4919e755debdcdb2957e53ae581afef4e
LUCAS DOS SANTOS COSTA <lucas.costa500@etec.sp.gov.br>
2025-08-14 21:39:46 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+$cidade["SP"][1] = "Jundiaí";
+$cidade["SP"][2] = "São Paulo";
+$cidade["BA"][1] = "Porto Seguro";
+$cidade["MG"][1] = "Porto Alegre";
+
+echo $cidade["SP"] [1] . "<br>"; //resultado: Jundiaí
+echo $cidade["BA"] [1] . "<br>"; //resultado: Porto Seguro
+echo $cidade["MG"] [1] . "<br>"; //resultado: Porto Alegre
+?>
---
+++ vetores/index.php
@@ -0,0 +1,12 @@
+<?php
+//Adicionando novos elementos
+$numeros[4] = 10;
+$numeros[11] = 37;
+$numeros[ ] = 100;
+
+echo $numeros[4] . "<br>"; //resultado:1
+echo $numeros[5] . "<br>"; //sem resultado
+echo $numeros[11] . "<br>"; //resultado: 37
+echo $numeros[12] . "<br>"; // 
+
+?>
